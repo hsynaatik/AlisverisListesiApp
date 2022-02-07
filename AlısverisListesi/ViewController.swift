a8eb27c9f0fdde7db4b05348fe41029369d21b71
hsynaatik <hsynaatik@gmail.com>
2022-02-07 00:56:28 -0800

    Initial Commit

---
+++ AlısverisListesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlısverisListesi
+//
+//  Created by Huseyin Atik on 7.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
